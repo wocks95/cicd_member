8a3ee79fa06ca40b6c5663549dd3270b6f81a30a
wocks95 <rla1995cks@naver.com>
2025-02-04 12:28:48 +0900

    20250204

--- src/test/java/com/min/memberapp/ApplicationTests.java
+++ src/test/java/com/min/memberapp/ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.min.memberapp;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.min.memberapp.mapper.IMemberMapper;
 
 @SpringBootTest
 class ApplicationTests {
 
+  @Autowired
+  IMemberMapper memberMapper;
 	@Test
-	void contextLoads() {
+	void select_member_test() {
+	  int memId = 1;
+	  Assertions.assertThat(memberMapper.selectMemberById(memId).getMemName()).isEqualTo("Gimblett");
 	}
 
 }
